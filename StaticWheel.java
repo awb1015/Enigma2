fe041d44833d875ec2f8149c2928473116bb1c54
Alex <alexander.w.baer@gmail.com>
2017-03-17 11:04:08 +0000

    Added a static wheel class
    Added a static wheel class, uses inheritance from the wheel. This just
    overides the Step method.

--- StaticWheel.java
+++ StaticWheel.java
@@ -1,5 +1,10 @@
 
 public class StaticWheel extends Wheel {
+	
+	public StaticWheel(String charOrder){
+		super(charOrder);
+	}
+	
 	public void step(){
 		//Over ride the default method here, no need to step the static wheel
 	}
