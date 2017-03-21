86f1f17aa34228cab7cc437f5bd85eedf3220050
Alex <alexander.w.baer@gmail.com>
2017-03-21 20:26:06 +0000

    Minor Additions
    Small adds to begin to create a plug class. Need to decide if there is a
    single plug object with a BiMap or just every plug create is its own
    object. BiMap with a single plug option may be easier to work with as it
    is not a progression for chars but rather a change and if the char isn't
    in the BiMap it is returned as normal to transit the wheels

---
+++ Plugs.java
@@ -0,0 +1,21 @@
+
+public class Plugs {
+	private char a;
+	private char b;
+	
+	
+	public Plugs(char a char b){
+		a = this.a;
+		b = this.b
+		
+	}
+	
+	public char getForwardPlug(char b){
+		
+	}
+	
+	public char getReversePlug(){
+		
+	}
+
+}
